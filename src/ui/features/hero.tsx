d1790357bca663b4908f2c344e2dcc8cce57d2d1
yoyobojo <angellolazar@gmail.com>
2023-03-03 16:34:52 -0500

    changed colors

--- src/ui/features/hero.tsx
+++ src/ui/features/hero.tsx
@@ -3,32 +3,32 @@ import Image from 'next/image';
 import { Background } from '../layouts/background';
 
 const Hero = () => (
-  <Background color="bg-stone-200">
+  <Background color="bg-amber-100">
     <div className="flex flex-col items-center justify-center gap-4 h-screen">
       <Image
-        className="absolute animate-spin-slow mb-5 hidden lg:block opacity-90"
-        src="/assets/images/circle-pattern.png"
+        className="absolute animate-spin-slow hidden lg:block mb-20"
+        src="/assets/images/circle-pattern-red.png"
         alt="Aztec"
         height="700"
         width="700"
       />
       <Image
-        className="rounded-md z-10"
+        className="rounded-md z-10 mb-20"
         src="/assets/images/hero-logo.png"
         alt="Teixiptla Holdings Logo"
         height="300"
         width="300"
       />
-      <h1 className="text-xl font-semibold z-10">Teixiptla Holdings LLC</h1>
     </div>
 
-    <div className="absolute flex flex-col gap-2 bottom-2 text-center justify-center items-center text-xs w-full mx-auto">
-      <span>
+    <div className="absolute flex flex-col gap-2 bottom-2 text-center justify-center items-center w-full mx-auto">
+      <span className="font-semibold">Teixiptla Holdings LLC</span>
+      <span className="text-xs">
         120 Madeira Drive NE, STE 220
         <br /> Albuquerque, New Mexico 87108
       </span>
-      <span>legal@teixiptla.holdings</span>
-      <span>+1-737-279-4407</span>
+      <span className="text-xs">legal@teixiptla.holdings</span>
+      <span className="text-xs">+1-737-279-4407</span>
     </div>
   </Background>
 );
